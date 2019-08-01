3327adfeeebcfb680692e918bda728823c629991
=DamjanRadev <radevdamjan@gmail.com>
2019-08-01 22:50:49 +0200

    Added BurgerIngredient component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import Aux from "../../hoc/Aux";
+
+class BurgerBulder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBulder;
