31ed82664f203bf3fbe83e2b85350aa171b63c15
=DamjanRadev <radevdamjan@gmail.com>
2019-09-06 16:24:50 +0200

    Added auth component for user auth

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Input from "../../components/UI/Input/Input";
+
+class Auth extends Component {
+  state = {
+    controls: 
+  }
+  render() {
+    return (
+      <div>
+        <form></form>
+      </div>
+    );
+  }
+}
+
+export default Auth;
